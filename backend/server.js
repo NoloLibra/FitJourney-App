6fa530a9d6bcda27f614cd0ca91515c6b1c99f3d
LibraNolo <bnkosi0312@gmail.com>
2024-11-30 00:55:32 +0200

    Fix serverless function setup

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,28 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose')
-const goalsRoutes = require('./routes/goals')
+const mongoose = require('mongoose');
+const goalsRoutes = require('./routes/goals');
 
-const app = express(); //creating an express app
+const app = express();
 
-//middleware
+// Middleware
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
+  console.log(req.path, req.method);
+  next();
+});
 
-//routes
-app.use(express.json())
-app.use('/api/goals', goalsRoutes)
-// connect to db 
+// Routes
+app.use(express.json());
+app.use('/api/goals', goalsRoutes);
+
+// Connect to DB
 mongoose.connect(process.env.MONG_URI)
-.then(() => {app.listen(process.env.PORT, () => {
-    console.log('connected to DB listening on PORT', process.env.PORT)  //listening for HTTP requests
-})})
-.catch((error) => {
-console.log(error)
-})
+  .then(() => {
+    console.log('Connected to DB');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
+// Export the app for serverless
+module.exports = app;
